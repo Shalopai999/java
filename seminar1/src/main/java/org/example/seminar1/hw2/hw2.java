e39e0b864ce9f02e1dd399a5f3c0110a37bc09fd
Andrey Nepomnyaschikh <And-dk@mail.ru>
2023-02-14 22:32:11 +0700

    hw2

--- seminar1/src/main/java/org/example/seminar1/hw2/hw2.java
+++ seminar1/src/main/java/org/example/seminar1/hw2/hw2.java
@@ -1,4 +1,17 @@
 package org.example.seminar1.hw2;
 
+import java.util.Scanner;
+
 public class hw2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str1 = "the sky is blue";
+        String[] strArray = str1.split(" ");
+        for (int i = strArray.length; i > 0; i--){
+            System.out.print(strArray[i-1] + ' ');
+
+        }
+
+    }
+
 }
